29f2a733ca04492a622f23910c54769fbc76f1cb
adorno-dev <adorno.dev@gmail.com>
2024-05-02 22:03:36 -0300

    include filetype on lsp

--- lua/roslyn/client.lua
+++ lua/roslyn/client.lua
@@ -93,6 +93,7 @@ function M.spawn(cmd, target, settings, on_exit, on_attach, capabilities)
 		name = "roslyn",
 		capabilities = capabilities,
 		settings = settings,
+        filetypes = { "cs" },
 		-- cmd = hacks.wrap_server_cmd(vim.lsp.rpc.connect("127.0.0.1", 8080)),
 		cmd = hacks.wrap_server_cmd(roslyn_lsp_rpc.start_uds(cmd, server_args)),
 		root_dir = vim.fn.getcwd(), ---@diagnostic disable-line: assign-type-mismatch
